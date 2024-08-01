6d85c066f79591b0c8a45dd49f1470d01bc52c74
kijeong <ykjmove@gmail.com>
2024-08-02 02:01:25 +0900

    ♻️ : 취약성 문제로 @nestjs-modules/mailer 제거 #15

--- src/modules/mail.module.ts
+++ src/modules/mail.module.ts
@@ -1,33 +1,27 @@
 import { Module } from '@nestjs/common';
-import { MailService } from '../services/mail.service';
-import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
+import { MailService } from 'src/services/mail.service';
 
 @Module({
-  imports: [
-    MailerModule.forRootAsync({
+  providers: [
+    MailService,
+    {
+      provide: 'MAIL_TRANSPORTER',
       useFactory: (configService: ConfigService) => {
-        const user = configService.get<string>('MAIL_USER');
-        const pass = configService.get<string>('MAIL_PW');
-        const from = configService.get<string>('MAIL_FROM');
-        return {
-          transport: {
-            host: 'smtp.naver.com',
-            port: 587,
-            auth: {
-              user,
-              pass,
-            },
+        return nodemailer.createTransport({
+          host: 'smtp.naver.com',
+          port: 587,
+          secure: false,
+          auth: {
+            user: configService.get<string>('MAIL_USER'),
+            pass: configService.get<string>('MAIL_PW'),
           },
-          defaults: {
-            from: `승리요정 인증코드 <${from}>`,
-          },
-        };
+        });
       },
       inject: [ConfigService],
-    }),
+    },
   ],
   exports: [MailService],
-  providers: [MailService],
 })
 export class MailModule {}
--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,16 +1,23 @@
-import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class MailService {
-  constructor(private readonly mailerService: MailerService) {}
+  constructor(
+    @Inject('MAIL_TRANSPORTER')
+    private readonly mailerService: nodemailer.Transporter,
+    private readonly configService: ConfigService,
+  ) {}
 
-  async sendAuthCodeMail(email: string, code: string) {
+  async sendAuthCodeMail(email: string, code: string): Promise<boolean> {
     try {
+      const fromAddress = this.configService.get('MAIL_FROM');
       await this.mailerService.sendMail({
-        to: `${email}`,
+        from: `승리요정 <${fromAddress}>`,
+        to: email,
         subject: '승리요정 인증 코드 번호입니다.',
-        text: `인증 코드  :  ${code}`,
+        text: `인증 코드: ${code}`,
       });
       return true;
     } catch (error) {
