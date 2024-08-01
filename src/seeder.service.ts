b77218d0bf6eb4a380f76184fe28c79016e426c9
Heo Daewoong <princewoong@gmail.com>
2024-08-01 19:13:49 +0900

    ✨ 앱 시작 시 크롤링 한 번 실행

--- src/seeder.service.ts
+++ src/seeder.service.ts
@@ -1,16 +1,20 @@
 import { Injectable, OnApplicationBootstrap } from "@nestjs/common";
 import { TeamService } from "./services/team.service";
 import { StadiumService } from "./services/stadium.service";
+import { BatchService } from "./services/batch.service";
 
 @Injectable()
 export class SeederService implements OnApplicationBootstrap {
   constructor(
     private readonly teamService: TeamService,
     private readonly stadiumService: StadiumService,
+    private readonly batchService: BatchService,
   ) {}
   
   onApplicationBootstrap() {
     this.teamService.seed();
     this.stadiumService.seed();
+    
+    this.batchService.batchUpdateGames();
   }
 }
