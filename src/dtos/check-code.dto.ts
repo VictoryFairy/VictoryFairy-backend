7fe778715fdcd77bbfc53c6fc15318a8a6ab0542
kijeong <ykjmove@gmail.com>
2024-08-01 20:16:51 +0900

    ♻️ : const 변수로 변경 #15

--- src/dtos/check-code.dto.ts
+++ src/dtos/check-code.dto.ts
@@ -1,12 +1,13 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { IsString, Length } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
+import { CODE_LENGTH } from 'src/const/auth.const';
 
 export class EmailWithCodeDto extends PickType(CreateUserDto, [
   'email',
 ] as const) {
   @ApiProperty()
   @IsString()
-  @Length(5, 5)
+  @Length(CODE_LENGTH, CODE_LENGTH)
   code: string;
 }
