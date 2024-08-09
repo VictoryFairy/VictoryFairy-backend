7020214c248d3d9f9be8a1242a60de72640d2363
Heo Daewoong <princewoong@gmail.com>
2024-08-09 18:20:34 +0900

    📝 DTO API 문서 업데이트

--- src/dtos/game.dto.ts
+++ src/dtos/game.dto.ts
@@ -6,57 +6,71 @@ import { StadiumDto } from './stadium.dto';
 
 @Exclude()
 export class GameDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: '경기 ID',
+    example: '20240801SSLG0'
+  })
   @IsNumber()
   @Expose()
-  id: number;
+  id: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '경기 일자',
+    example: '2024-08-01',
+  })
   @IsString()
   @Expose()
-  date: Date;
+  date: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '경기 시작 시간',
+    example: '18:30:00',
+  })
   @IsString()
   @Expose()
   time: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '경기 상태',
+    example: '경기 종료',
+  })
   @IsString()
   @Expose()
   status: string;
 
   @ApiProperty()
-  @IsString()
   @Expose()
   @Transform(({ obj }) => obj.home_team)
   homeTeam: TeamDto;
 
   @ApiProperty()
-  @IsString()
   @Expose()
   @Transform(({ obj }) => obj.away_team)
   awayTeam: TeamDto;
 
   @ApiProperty()
-  @IsNumber()
   @Expose()
   stadium: StadiumDto;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: '홈 팀 점수',
+    example: 0,
+  })
   @IsString()
   @Expose()
   @Transform(({ obj }) => obj.home_team_score)
   homeTeamScore?: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: '어웨이 팀 점수',
+    example: 7,
+  })
   @IsString()
   @Expose()
   @Transform(({ obj }) => obj.away_team_score)
   awayTeamScore?: string;
 
   @ApiPropertyOptional()
-  @IsString()
   @Expose()
   winningTeam?: TeamDto;
 }
--- src/dtos/registered-game.dto.ts
+++ src/dtos/registered-game.dto.ts
@@ -1,87 +1,76 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional, OmitType, PartialType } from '@nestjs/swagger';
 import { Exclude, Expose, Transform } from 'class-transformer';
 import { IsNumber, IsString } from 'class-validator';
 
-export class CreateRegisteredGameDto {
-  @ApiProperty()
-  @IsString()
-  image: string;
-
-  @ApiProperty()
-  @IsString()
-  seat: string;
-
-  @ApiProperty()
-  @IsString()
-  review: string;
-
-  @ApiProperty()
-  @IsString()
-  gameId: string;
-
-  @ApiProperty()
-  @IsNumber()
-  cheeringTeamId: number;
-}
-
-export class UpdateRegisteredGameDto {
-  @ApiPropertyOptional()
-  @IsString()
-  image?: string;
-
-  @ApiPropertyOptional()
-  @IsString()
-  seat?: string;
-
-  @ApiPropertyOptional()
-  @IsString()
-  review?: string;
-
-  @ApiPropertyOptional()
-  @IsNumber()
-  cheeringTeamId: number;
-}
-
 @Exclude()
 export class RegisteredGameDto {
+  @ApiProperty({
+    description: '등록 직관 경기 ID',
+    example: 1,
+  })
   @IsNumber()
   @Expose()
   id: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '',
+    example: 'http://example.com/url/to/image.jpg'
+  })
   @IsString()
   @Expose()
   image: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '좌석 상세',
+    example: '115블록 2열 13번'
+  })
   @IsString()
   @Expose()
   seat: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '감상평',
+    example: '좋았다'
+  })
   @IsString()
   @Expose()
   review: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '연결된 경기의 ID',
+    example: '20240801SSLG0'
+  })
   @IsString()
   @Expose()
   @Transform(({ obj }) => obj.game.id)
   gameId: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '응원하는 팀의 ID',
+    example: 4,
+  })
   @IsString()
   @Expose()
   @Transform(({ obj }) => obj.cheering_team.id)
   cheeringTeamId: number;
 }
 
+export class CreateRegisteredGameDto extends OmitType(RegisteredGameDto, ['id'] as const) {}
+
+export class UpdateRegisteredGameDto extends OmitType(PartialType(CreateRegisteredGameDto), ['gameId'] as const) {}
+
 export class FindAllMonthlyQueryDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: '년도',
+    example: 2024,
+  })
   @IsNumber()
   year: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '월',
+    example: 8,
+  })
   @IsNumber()
   month: number;
 }
