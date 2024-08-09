d4370a0a074b03e9b5dfab68d0dabf6012df74bf
Heo Daewoong <princewoong@gmail.com>
2024-08-09 16:21:20 +0900

    📝 경기 API 문서 상세화

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -6,7 +6,7 @@ import {
   Param,
   Query,
 } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
 import { FindAllDailyQueryDto, GameDto } from 'src/dtos/game.dto';
 import { GameService } from 'src/services/game.service';
@@ -18,7 +18,9 @@ export class GameController {
 
   @Get('daily')
   @HttpCode(HttpStatus.OK)
-  @ApiOkResponse({ type: [GameDto] })
+  @ApiOperation({ summary: '하루 동안의 경기 목록 반환' })
+  @ApiQuery({ type: FindAllDailyQueryDto, description: '년, 월, 일 정보' })
+  @ApiOkResponse({ type: [GameDto], description: '데이터가 없더라도 오류 없이 빈 배열 반환' })
   async findAllDaily(@Query() query: FindAllDailyQueryDto): Promise<GameDto[]> {
     const { year, month, day } = query;
     const games = await this.gameService.findAllDaily(year, month, day);
@@ -27,7 +29,10 @@ export class GameController {
 
   @Get(':id')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'ID로 경기 반환' })
+  @ApiParam({ name: 'id', type: String, description: '경기의 ID' })
   @ApiOkResponse({ type: GameDto })
+  @ApiNotFoundResponse({ description: '해당 ID의 경기가 없을 경우' })
   async findOne(@Param('id') id: string): Promise<GameDto> {
     const game = await this.gameService.findOne(id);
     return plainToInstance(GameDto, game);
