153511c240264e3de7a039819e21d631b0f2cd96
kijeong <ykjmove@gmail.com>
2024-08-12 14:46:13 +0900

    🔧 : drop schema 빼기

--- src/services/seeder.service.ts
+++ src/services/seeder.service.ts
@@ -2,7 +2,6 @@ import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { TeamService } from './team.service';
 import { StadiumService } from './stadium.service';
 import { SchedulingService } from './scheduling.service';
-import { UserService } from './user.service';
 import { ParkingInfoService } from './parking-info.service';
 import { CheeringSongService } from './cheering-song.service';
 
@@ -12,7 +11,6 @@ export class SeederService implements OnApplicationBootstrap {
     private readonly teamService: TeamService,
     private readonly stadiumService: StadiumService,
     private readonly schedulingService: SchedulingService,
-    private readonly userService: UserService,
     private readonly parkingInfoService: ParkingInfoService,
     private readonly cheeringSongService: CheeringSongService,
   ) {}
@@ -20,7 +18,6 @@ export class SeederService implements OnApplicationBootstrap {
   async onApplicationBootstrap() {
     await this.teamService.seed();
     await this.stadiumService.seed();
-    await this.userService.seed();
     await this.parkingInfoService.seed();
     await this.cheeringSongService.seed();
 
