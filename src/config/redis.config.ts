44b40afec035b4ce972bb5843acd47f4076a0ed4
kijeong <ykjmove@gmail.com>
2024-08-15 12:09:15 +0900

    🔧 : 레디스 연결 시도 전략 수정 #31

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -6,11 +6,7 @@ export const redisConfig = (configService: ConfigService) => {
     host: configService.get<string>('REDIS_CONTAINER') || 'localhost',
     port: parseInt(configService.get<string>('REDIS_TCP_PORT')),
     password: configService.get<string>('REDIS_PASSWORD'),
-    retryStrategy: (times) => {
-      // 첫 시도 1초 ... 마지막 시도 6초
-      const delay = Math.min(times * 1000, 6000);
-      return delay;
-    },
+    retryStrategy: (times) => times * 1000,
   };
 
   return new Redis(options);
