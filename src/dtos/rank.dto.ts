4207b86829818f5e5d1f84f55b20b1be03cb6561
kijeong <ykjmove@gmail.com>
2024-08-20 16:38:39 +0900

    ♻️ : Ranking dto 생성 및 dto에 스웨거 예시 이동

--- src/dtos/rank.dto.ts
+++ src/dtos/rank.dto.ts
@@ -90,3 +90,73 @@ export class ResRankDto {
   @Transform(({ obj }) => obj.user_id ?? obj.userId)
   userId: number;
 }
+
+export class ResRankTopThreeDto {
+  @ApiProperty({
+    type: [ResRankDto],
+    example: [
+      {
+        rank: 1,
+        score: 1025,
+        image: 'imagefadfafa',
+        nickname: 'test1',
+        userId: 2,
+      },
+      {
+        rank: 2,
+        score: 1020,
+        image: 'imagefadfafa',
+        nickname: 'test4',
+        userId: 5,
+      },
+      {
+        rank: 3,
+        score: 1010,
+        image: 'imagefadfafa',
+        nickname: 'test10',
+        userId: 11,
+      },
+    ],
+  })
+  top: ResRankDto[];
+}
+
+class UserStatsDto {
+  @ApiProperty({ example: 2 })
+  userId: number;
+
+  @ApiProperty({ example: 8 })
+  totalGames: number;
+
+  @ApiProperty({ example: 6 })
+  win: number;
+}
+
+export class ResNearByDto {
+  @ApiProperty({
+    type: ResRankDto,
+    example: [
+      {
+        rank: 1,
+        score: 1025,
+        image: 'imagefadfafa',
+        nickname: 'test1',
+        userId: 2,
+      },
+      {
+        rank: 2,
+        score: 1020,
+        image: 'imagefadfafa',
+        nickname: 'test4',
+        userId: 5,
+      },
+    ],
+  })
+  nearBy: ResRankDto[];
+
+  @ApiProperty({
+    type: UserStatsDto,
+    example: { userId: 2, totalGames: 8, win: 6 },
+  })
+  user: UserStatsDto;
+}
