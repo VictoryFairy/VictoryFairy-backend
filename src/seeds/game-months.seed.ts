cededbe518097c5fc3c800c5f19d14f52d551ae9
Heo Daewoong <princewoong@gmail.com>
2024-08-23 21:43:23 +0900

    🌱 2023, 2024년 게임 정보 시드 추가

---
+++ src/seeds/game-months.seed.ts
@@ -0,0 +1,98 @@
+export const gameMonths: { year: number; month: number }[] = [
+  {
+    year: 2023,
+    month: 1,
+  },
+  {
+    year: 2023,
+    month: 2,
+  },
+  {
+    year: 2023,
+    month: 3,
+  },
+  {
+    year: 2023,
+    month: 4,
+  },
+  {
+    year: 2023,
+    month: 5,
+  },
+  {
+    year: 2023,
+    month: 6,
+  },
+  {
+    year: 2023,
+    month: 7,
+  },
+  {
+    year: 2023,
+    month: 8,
+  },
+  {
+    year: 2023,
+    month: 9,
+  },
+  {
+    year: 2023,
+    month: 10,
+  },
+  {
+    year: 2023,
+    month: 11,
+  },
+  {
+    year: 2023,
+    month: 12,
+  },
+  {
+    year: 2024,
+    month: 1,
+  },
+  {
+    year: 2024,
+    month: 2,
+  },
+  {
+    year: 2024,
+    month: 3,
+  },
+  {
+    year: 2024,
+    month: 4,
+  },
+  {
+    year: 2024,
+    month: 5,
+  },
+  {
+    year: 2024,
+    month: 6,
+  },
+  {
+    year: 2024,
+    month: 7,
+  },
+  {
+    year: 2024,
+    month: 8,
+  },
+  {
+    year: 2024,
+    month: 9,
+  },
+  {
+    year: 2024,
+    month: 10,
+  },
+  {
+    year: 2024,
+    month: 11,
+  },
+  {
+    year: 2024,
+    month: 12,
+  },
+];
