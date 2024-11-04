0bd8ef3d07cb470aba6a2a26fe84f8b881b81e21
kijeong <ykjmove@gmail.com>
2024-11-04 21:27:13 +0900

    ♻️ : RedisKey를 RedisCaching서비스 파일에 합치기

--- src/const/redis.const.ts
+++
@@ -1,8 +0,0 @@
-export enum RedisKeys {
-  /** @description [Namespace 없음] */
-  USER_INFO = 'userInfo',
-  /** @description [Namespace 필수] Email */
-  EMAIL_CODE = 'code',
-  /** @description [Namespace 필수] 전체 : total, 팀 : teamId */
-  RANKING = 'rank',
-}
