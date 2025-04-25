817d63c193f948a5bcdae138eba57a3ebc593f80
kijeong <ykjmove@gmail.com>
2025-04-25 20:04:52 +0900

    ✏️ : 팀명 변경하고

--- src/tools/seeds/data/team.seed.ts
+++ src/tools/seeds/data/team.seed.ts
@@ -1,16 +1,16 @@
 import { TTeam } from 'src/modules/scheduling/crawling-game.type';
 
 export const teamSeeder: { name: TTeam }[] = [
-  { name: '롯데' },
-  { name: '두산' },
-  { name: 'KIA' },
-  { name: '삼성' },
-  { name: 'SSG' },
-  { name: 'NC' },
-  { name: 'LG' },
-  { name: '키움' },
-  { name: 'KT' },
-  { name: '한화' },
+  { name: '롯데 자이언츠' },
+  { name: '두산 베어스' },
+  { name: 'KIA 타이거즈' },
+  { name: '삼성 라이온즈' },
+  { name: 'SSG 랜더스' },
+  { name: 'NC 다이노스' },
+  { name: 'LG 트윈스' },
+  { name: '키움 히어로즈' },
+  { name: 'KT 위즈' },
+  { name: '한화 이글스' },
   { name: '나눔' },
   { name: '드림' },
 ];
