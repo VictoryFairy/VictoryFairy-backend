c00c332b4c4693c2624a45899574a59d1c150545
kijeong <ykjmove@gmail.com>
2025-05-27 19:03:33 +0900

    ✨ : sentry 연결

--- src/core/slack/slack.service.ts
+++ src/core/slack/slack.service.ts
@@ -14,17 +14,13 @@ export class SlackService {
   async sendInternalErrorNotification(
     message: string,
     errorName: string,
-    errorStack: string,
   ): Promise<void> {
     await this.webhook.send({
       text: `에러 발생 : ${message}`,
       attachments: [
         {
           color: 'danger',
-          fields: [
-            { title: '에러 이름', value: errorName },
-            { title: '에러 스택', value: errorStack },
-          ],
+          fields: [{ title: '에러 이름', value: errorName }],
         },
       ],
     });
