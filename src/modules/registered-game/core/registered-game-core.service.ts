88d725d7aa8c166523f1f0a1b905afb9f39e9de3
kijeong <ykjmove@gmail.com>
2025-12-02 23:49:38 +0900

    ♻️ : registered-game 코어 모듈, 서비스 작성 및 엔티티에 도메인 메소드로 작성

---
+++ src/modules/registered-game/core/registered-game-core.service.ts
@@ -0,0 +1,162 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
+import { RegisteredGame } from '../entities/registered-game.entity';
+import { SaveRegisteredGameDto } from '../dto/internal/save-registered-game.dto';
+import { UpdateRegisteredGameDto } from '../dto/request/req-update-registered-game.dto';
+import { RegisteredGameStatus } from '../types/registered-game-status.type';
+import { DeleteRegisteredGameDto } from '../dto/internal/delete-registered-game.dto';
+
+@Injectable()
+export class RegisteredGameCoreService {
+  constructor(
+    @InjectRepository(RegisteredGame)
+    private readonly registeredGameRepo: Repository<RegisteredGame>,
+  ) {}
+
+  async getByGameId(gameId: string, where?: FindOptionsWhere<RegisteredGame>) {
+    const registeredGame = await this.registeredGameRepo.find({
+      where: { game: { id: gameId }, ...where },
+      relations: {
+        game: { home_team: true, away_team: true, winning_team: true },
+        cheering_team: true,
+        user: true,
+      },
+    });
+    return registeredGame;
+  }
+  async saveBulk(data: RegisteredGame[]) {
+    const savedRegisteredGames = await this.registeredGameRepo.save(data);
+    return savedRegisteredGames;
+  }
+  async save(data: SaveRegisteredGameDto): Promise<RegisteredGame> {
+    const { image, seat, review, game, cheeringTeam, user } = data;
+
+    const isDuplicate = await this.registeredGameRepo.findOne({
+      where: { game: { id: game.id }, user: { id: user.id } },
+    });
+    if (isDuplicate) {
+      throw new ConflictException('이미 등록된 경기입니다');
+    }
+
+    const newRegisteredGame = RegisteredGame.create({
+      image,
+      seat,
+      review,
+      userId: user.id,
+      cheeringTeamId: cheeringTeam.id,
+      gameMetaData: {
+        gameId: game.id,
+        homeTeamId: game.homeTeam.id,
+        awayTeamId: game.awayTeam.id,
+      },
+    });
+
+    newRegisteredGame.determineStatus(game.status, cheeringTeam.id);
+
+    const savedRegisteredGame =
+      await this.registeredGameRepo.save(newRegisteredGame);
+    return savedRegisteredGame;
+  }
+
+  async update(
+    id: number,
+    dto: UpdateRegisteredGameDto,
+    userId: number,
+  ): Promise<{
+    imageCtx: {
+      isChanged: boolean;
+      prevImage: string | null;
+    };
+    teamCtx: {
+      isChanged: boolean;
+      prevTeamId: number;
+      newTeamId: number;
+      prevStatus: RegisteredGameStatus | null;
+      changedStatus: RegisteredGameStatus | null;
+      year: number;
+    };
+  }> {
+    const { cheeringTeamId, seat, review, image } = dto;
+
+    const registeredGame = await this.registeredGameRepo.findOne({
+      where: { id, user: { id: userId } },
+      relations: {
+        game: { home_team: true, away_team: true },
+        cheering_team: true,
+      },
+    });
+    if (!registeredGame) {
+      throw new NotFoundException(
+        '직관 경기가 존재하지 않아서 수정할 수 없습니다.',
+      );
+    }
+
+    const prevImage = registeredGame.image;
+    const prevTeamId = registeredGame.cheering_team.id;
+    const prevStatus = registeredGame.status;
+
+    registeredGame.updateImage(image);
+    registeredGame.updateReviewAndSeat(review, seat);
+    registeredGame.updateCheeringTeam({
+      cheeringTeamId,
+      homeTeamId: registeredGame.game.home_team.id,
+      awayTeamId: registeredGame.game.away_team.id,
+    });
+    await this.registeredGameRepo.save(registeredGame);
+
+    const result = {
+      imageCtx: { isChanged: prevImage !== image, prevImage },
+      teamCtx: {
+        isChanged: prevTeamId !== cheeringTeamId,
+        prevTeamId: prevTeamId,
+        newTeamId: cheeringTeamId,
+        prevStatus,
+        changedStatus: registeredGame.status,
+        year: new Date(registeredGame.game.date).getFullYear(),
+      },
+    };
+
+    return result;
+  }
+
+  async delete(dto: DeleteRegisteredGameDto): Promise<{
+    imageCtx: {
+      prevImage: string | null;
+    };
+    gameCtx: {
+      status: RegisteredGameStatus | null;
+      teamId: number;
+      year: number;
+    };
+  }> {
+    const { RegisteredGameId, userId } = dto;
+    const registeredGame = await this.registeredGameRepo.findOne({
+      where: { id: RegisteredGameId, user: { id: userId } },
+      relations: {
+        game: { home_team: true, away_team: true },
+        cheering_team: true,
+      },
+    });
+
+    if (!registeredGame) {
+      throw new NotFoundException(
+        '직관 경기가 존재하지 않아서 삭제할 수 없습니다.',
+      );
+    }
+
+    const prevImage = registeredGame.image;
+    const gameCtx = {
+      status: registeredGame.status,
+      teamId: registeredGame.cheering_team.id,
+      year: new Date(registeredGame.game.date).getFullYear(),
+    };
+
+    await this.registeredGameRepo.remove(registeredGame);
+    return { imageCtx: { prevImage }, gameCtx };
+  }
+}
--- src/modules/registered-game/entities/registered-game.entity.ts
+++ src/modules/registered-game/entities/registered-game.entity.ts
@@ -11,7 +11,7 @@ import {
 import { Team } from '../../team/entities/team.entity';
 import { RegisteredGameStatus } from 'src/modules/registered-game/types/registered-game-status.type';
 import { Game } from '../../game/entities/game.entity';
-import { User } from '../../user/entities/user.entity';
+import { User } from 'src/modules/account/core/domain/user.entity';
 
 @Entity()
 @Unique(['game', 'user'])
@@ -50,4 +50,125 @@ export class RegisteredGame {
   @ManyToOne(() => Team, (cheering_team) => cheering_team.registeredGames)
   @JoinColumn({ name: 'cheering_team_id' })
   cheering_team: Team;
+
+  private constructor() {}
+
+  static create(props: {
+    image: string | null;
+    seat: string;
+    review: string;
+    userId: number;
+    cheeringTeamId: number;
+    gameMetaData: {
+      gameId: string;
+      homeTeamId: number;
+      awayTeamId: number;
+    };
+  }) {
+    const { gameId, homeTeamId, awayTeamId } = props.gameMetaData;
+
+    const registeredGame = new RegisteredGame();
+    registeredGame.image = props.image;
+    registeredGame.seat = props.seat;
+    registeredGame.review = props.review;
+
+    registeredGame.game = { id: gameId } as Game;
+    registeredGame.user = { id: props.userId } as User;
+    registeredGame.cheering_team = { id: props.cheeringTeamId } as Team;
+
+    registeredGame.validateDomain();
+    registeredGame.validateCheeringTeam({ homeTeamId, awayTeamId });
+    return registeredGame;
+  }
+
+  updateReviewAndSeat(review: string, seat: string) {
+    if (this.review === review && this.seat === seat) {
+      return;
+    }
+    this.review = review;
+    this.seat = seat;
+  }
+
+  updateImage(image: string): void {
+    if (this.image === image) {
+      return;
+    }
+    this.image = image;
+  }
+
+  updateCheeringTeam(data: {
+    cheeringTeamId: number;
+    homeTeamId: number;
+    awayTeamId: number;
+  }) {
+    const { cheeringTeamId, homeTeamId, awayTeamId } = data;
+    if (this.cheering_team.id === data.cheeringTeamId) {
+      return;
+    }
+    const previousCheeringTeamId = this.cheering_team.id;
+    if (previousCheeringTeamId !== cheeringTeamId) {
+      this.cheering_team = { id: cheeringTeamId } as Team;
+      this.validateCheeringTeam({ homeTeamId, awayTeamId });
+      this.changeStatusWhenCheeringTeamChanged();
+    }
+  }
+
+  changeStatusWhenCheeringTeamChanged() {
+    if (this.status === 'Win') {
+      this.status = 'Lose';
+    } else if (this.status === 'Lose') {
+      this.status = 'Win';
+    }
+  }
+
+  determineStatus(status: string, winnerTeamId: number | null) {
+    if (
+      /.*취소$/.test(status) ||
+      status === '그라운드사정' ||
+      status === '기타'
+    ) {
+      this.status = 'No game';
+      return;
+    }
+    if (status === '경기종료') {
+      if (!winnerTeamId) {
+        this.status = 'Tie';
+      }
+      this.status = winnerTeamId === this.cheering_team.id ? 'Win' : 'Lose';
+      return;
+    }
+    // 경기 중인 경우
+    this.status = null;
+    return;
+  }
+
+  private validateDomain() {
+    if (!this.user) {
+      throw new Error('User is required');
+    }
+    if (!this.game) {
+      throw new Error('Game is required');
+    }
+    if (!this.cheering_team.id) {
+      throw new Error('Cheering team id is required');
+    }
+    if (!this.seat) {
+      throw new Error('Seat is required');
+    }
+    if (!this.review) {
+      throw new Error('Review is required');
+    }
+  }
+
+  private validateCheeringTeam(gameData: {
+    homeTeamId: number;
+    awayTeamId: number;
+  }) {
+    if (
+      this.cheering_team.id !== gameData.homeTeamId &&
+      this.cheering_team.id !== gameData.awayTeamId
+    ) {
+      throw new Error('Cheering team is not the home or away team of the game');
+    }
+  }
 }
