e4e6cec176e5bb1e50a28b6503848c58ae0cb8b4
kijeong <ykjmove@gmail.com>
2025-12-09 23:15:30 +0900

    🔥 : 게임 모듈 및 서비스 파일 제거 - 불필요한 코드 정리

--- src/modules/stadium/stadium.module.ts
+++ src/modules/stadium/stadium.module.ts
@@ -3,9 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StadiumController } from 'src/modules/stadium/stadium.controller';
 import { Stadium } from 'src/modules/stadium/entities/stadium.entity';
 import { StadiumService } from 'src/modules/stadium/stadium.service';
-import { AuthModule } from '../auth/auth.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([Stadium]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Stadium])],
   controllers: [StadiumController],
   providers: [StadiumService],
   exports: [StadiumService],
